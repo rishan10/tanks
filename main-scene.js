a802162160259c8e38e35a595582c29b74a146b4
Rishan Girish <rishang10@gmail.com>
2019-11-17 17:10:35 -0800

    made wall

--- main-scene.js
+++ main-scene.js
@@ -2,8 +2,8 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
 class Assignment_Three_Scene extends Scene_Component
   { constructor( context, control_box )     // The scene begins by requesting the camera, shapes, and materials it will need.
       { super(   context, control_box );    // First, include a secondary Scene that provides movement controls:
-        if( !context.globals.has_controls   ) 
-          context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) ); 
+        if( !context.globals.has_controls   )
+          context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) );
 
         context.globals.graphics_state.camera_transform = Mat4.look_at( Vec.of( 0,10,20 ), Vec.of( 0,0,0 ), Vec.of( 0,1,0 ) );
         this.initial_camera_location = Mat4.inverse( context.globals.graphics_state.camera_transform );
@@ -12,13 +12,14 @@ class Assignment_Three_Scene extends Scene_Component
         context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1000 );
 
         const shapes = { torus:  new Torus( 15, 15 ),
-                         torus2: new ( Torus.prototype.make_flat_shaded_version() )( 15, 15 )
- 
+                         torus2: new ( Torus.prototype.make_flat_shaded_version() )( 15, 15 ),
+                         block : new Cube()
+
                                 // TODO:  Fill in as many additional shape instances as needed in this key/value table.
                                 //        (Requirement 1)
                        }
         this.submit_shapes( context, shapes );
-                                     
+
                                      // Make some Material objects available to you:
         this.materials =
           { test:     context.get_instance( Phong_Shader ).material( Color.of( 1,1,0,1 ), { ambient:.2 } ),
@@ -44,12 +45,23 @@ class Assignment_Three_Scene extends Scene_Component
       { graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
         const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
 
-        
+
 
         // TODO:  Fill in matrix operations and drawing code to draw the solar system scene (Requirements 2 and 3)
 
 
-        this.shapes.torus2.draw( graphics_state, Mat4.identity(), this.materials.test );
+
+        let model_transform = Mat4.identity();
+
+        for(var i = 0; i < 10; i++) {
+          for(var j = 0; j < 10; j++) {
+            this.shapes.block.draw(graphics_state, model_transform, this.materials.test);
+            model_transform= model_transform
+          .times( Mat4.translation([2,0,0]));
+          }
+          model_transform = model_transform
+        .times( Mat4.translation([-20, -2,0]));
+        }
 
       }
   }
@@ -62,10 +74,10 @@ class Ring_Shader extends Shader              // Subclasses of Shader each store
 { material() { return { shader: this } }      // Materials here are minimal, without any settings.
   map_attribute_name_to_buffer_name( name )       // The shader will pull single entries out of the vertex arrays, by their data fields'
     {                                             // names.  Map those names onto the arrays we'll pull them from.  This determines
-                                                  // which kinds of Shapes this Shader is compatible with.  Thanks to this function, 
-                                                  // Vertex buffers in the GPU can get their pointers matched up with pointers to 
+                                                  // which kinds of Shapes this Shader is compatible with.  Thanks to this function,
+                                                  // Vertex buffers in the GPU can get their pointers matched up with pointers to
                                                   // attribute names in the GPU.  Shapes and Shaders can still be compatible even
-                                                  // if some vertex data feilds are unused. 
+                                                  // if some vertex data feilds are unused.
       return { object_space_pos: "positions" }[ name ];      // Use a simple lookup table.
     }
     // Define how to synchronize our JavaScript's variables to the GPU's:
@@ -88,23 +100,47 @@ class Ring_Shader extends Shader              // Subclasses of Shader each store
         uniform mat4 projection_camera_transform;
 
         void main()
-        { 
+        {
         }`;           // TODO:  Complete the main function of the vertex shader (Extra Credit Part II).
     }
   fragment_glsl_code()           // ********* FRAGMENT SHADER *********
     { return `
         void main()
-        { 
+        {
         }`;           // TODO:  Complete the main function of the fragment shader (Extra Credit Part II).
     }
 }
 
 window.Grid_Sphere = window.classes.Grid_Sphere =
-class Grid_Sphere extends Shape           // With lattitude / longitude divisions; this means singularities are at 
+class Grid_Sphere extends Shape           // With lattitude / longitude divisions; this means singularities are at
   { constructor( rows, columns, texture_range )             // the mesh's top and bottom.  Subdivision_Sphere is a better alternative.
       { super( "positions", "normals", "texture_coords" );
-        
+
 
                       // TODO:  Complete the specification of a sphere with lattitude and longitude lines
                       //        (Extra Credit Part III)
       } }
+
+      window.Cube = window.classes.Cube =
+      class Cube extends Shape                 // Here's a complete, working example of a Shape subclass.  It is a blueprint for a cube.
+        { constructor()
+            { super( "positions", "normals" ); // Name the values we'll define per each vertex.  They'll have positions and normals.
+
+              // First, specify the vertex positions -- just a bunch of points that exist at the corners of an imaginary cube.
+              this.positions.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [-1,-1,1], [1,-1,1], [1,1,-1],  [-1,1,-1],  [1,1,1],  [-1,1,1],
+                                                [-1,-1,-1], [-1,-1,1], [-1,1,-1], [-1,1,1], [1,-1,1],  [1,-1,-1],  [1,1,1],  [1,1,-1],
+                                                [-1,-1,1],  [1,-1,1],  [-1,1,1],  [1,1,1], [1,-1,-1], [-1,-1,-1], [1,1,-1], [-1,1,-1] ) );
+              // Supply vectors that point away from eace face of the cube.  They should match up with the points in the above list
+              // Normal vectors are needed so the graphics engine can know if the shape is pointed at light or not, and color it accordingly.
+              this.normals.push(   ...Vec.cast( [0,-1,0], [0,-1,0], [0,-1,0], [0,-1,0], [0,1,0], [0,1,0], [0,1,0], [0,1,0], [-1,0,0], [-1,0,0],
+                                                [-1,0,0], [-1,0,0], [1,0,0],  [1,0,0],  [1,0,0], [1,0,0], [0,0,1], [0,0,1], [0,0,1],   [0,0,1],
+                                                [0,0,-1], [0,0,-1], [0,0,-1], [0,0,-1] ) );
+
+                       // Those two lists, positions and normals, fully describe the "vertices".  What's the "i"th vertex?  Simply the combined
+                       // data you get if you look up index "i" of both lists above -- a position and a normal vector, together.  Now let's
+                       // tell it how to connect vertex entries into triangles.  Every three indices in this list makes one triangle:
+              this.indices.push( 0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+                                14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22 );
+              // It stinks to manage arrays this big.  Later we'll show code that generates these same cube vertices more automatically.
+            }
+        }
