489ec56cd784e08e0f3123de6074267c561951ac
Rishan Girish <rishang10@gmail.com>
2019-11-20 18:29:12 -0800

    made floor

--- main-scene.js
+++ main-scene.js
@@ -1,3 +1,27 @@
+class Test_Data
+{                             // **Test_Data** pre-loads some Shapes and Textures that other Scenes can borrow.
+  constructor()
+  { this.textures = {
+    ground : new Texture( "asset/ground.png" ),
+
+  }
+    // this.shapes = { donut  : new defs.Torus          ( 15, 15, [[0,2],[0,1]] ),
+    //   cone   : new defs.Closed_Cone    ( 4, 10,  [[0,2],[0,1]] ),
+    //   capped : new defs.Capped_Cylinder( 4, 12,  [[0,2],[0,1]] ),
+    //   ball   : new defs.Subdivision_Sphere( 3,   [[0,1],[0,1]] ),
+    //   cube   : new defs.Cube(),
+    //   prism  : new ( defs.Capped_Cylinder   .prototype.make_flat_shaded_version() )( 10, 10, [[0,2],[0,1]] ),
+    //   gem    : new ( defs.Subdivision_Sphere.prototype.make_flat_shaded_version() )( 2 ),
+    //   donut2 : new ( defs.Torus             .prototype.make_flat_shaded_version() )( 20, 20, [[0,2],[0,1]] ),
+    // };
+  }
+  random_shape( shape_list = this.shapes )
+  {                                       // random_shape():  Extract a random shape from this.shapes.
+    const shape_names = Object.keys( shape_list );
+    return shape_list[ shape_names[ ~~( shape_names.length * Math.random() ) ] ]
+  }
+}
+
 window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
 class Assignment_Three_Scene extends Scene_Component
   { constructor( context, control_box )     // The scene begins by requesting the camera, shapes, and materials it will need.
@@ -13,7 +37,8 @@ class Assignment_Three_Scene extends Scene_Component
 
         const shapes = { torus:  new Torus( 15, 15 ),
                          torus2: new ( Torus.prototype.make_flat_shaded_version() )( 15, 15 ),
-                         block : new Cube()
+                         block : new Cube(),
+            square: new Square()
 
                                 // TODO:  Fill in as many additional shape instances as needed in this key/value table.
                                 //        (Requirement 1)
@@ -23,12 +48,14 @@ class Assignment_Three_Scene extends Scene_Component
                                      // Make some Material objects available to you:
         this.materials =
           { test:     context.get_instance( Phong_Shader ).material( Color.of( 1,1,0,1 ), { ambient:.2 } ),
-            ring:     context.get_instance( Ring_Shader  ).material()
+              earth: context.get_instance( Phong_Shader ).material(Color.of(0,0,0,1), {ambient: 1, texture: context.get_instance("assets/ground.jpeg", false)})
+            //ring:     context.get_instance( Ring_Shader  ).material()
 
                                 // TODO:  Fill in as many additional material objects as needed in this key/value table.
                                 //        (Requirement 1)
           }
 
+          //this.data = new Test_Data();
         this.lights = [ new Light( Vec.of( 5,-10,5,1 ), Color.of( 0, 1, 1, 1 ), 1000 ) ];
       }
     make_control_panel()            // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
@@ -41,6 +68,31 @@ class Assignment_Three_Scene extends Scene_Component
         this.key_triggered_button( "Attach to planet 5", [ "5" ], () => this.attached = () => this.planet_5 );
         this.key_triggered_button( "Attach to moon",     [ "m" ], () => this.attached = () => this.moon     );
       }
+
+    create_ground(graphics_state, model_transform) {
+        model_transform = model_transform.times( Mat4.translation( [0,-10,0] )).times( Mat4.rotation( Math.PI/2,   Vec.of(1,0,0) ) ).times( Mat4.scale( [50,50,1] ) );
+
+        this.shapes.square.draw( graphics_state,  model_transform ,
+            this.materials.earth);
+        model_transform = model_transform.times(Mat4.scale([1/50, 1/50,1])).times(Mat4.rotation(Math.PI/2, Vec.of(-1,0,0)))
+        model_transform = model_transform.times(Mat4.translation([0,1,0]))
+        return model_transform;
+    }
+
+    create_wall(graphics_state, model_transform) {
+        for(var i = 0; i < 10; i++) {
+            for(var j = 0; j < 10; j++) {
+                model_transform= model_transform
+                    .times( Mat4.translation([2,0,0]));
+                this.shapes.block.draw(graphics_state, model_transform, this.materials.test);
+            }
+            model_transform = model_transform
+                .times( Mat4.translation([-20, 2,0]));
+        }
+        return model_transform;
+
+    }
+
     display( graphics_state )
       { graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
         const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
@@ -49,98 +101,33 @@ class Assignment_Three_Scene extends Scene_Component
 
         // TODO:  Fill in matrix operations and drawing code to draw the solar system scene (Requirements 2 and 3)
 
-
-
-        let model_transform = Mat4.identity();
-
-        for(var i = 0; i < 10; i++) {
-          for(var j = 0; j < 10; j++) {
-            this.shapes.block.draw(graphics_state, model_transform, this.materials.test);
-            model_transform= model_transform
-          .times( Mat4.translation([2,0,0]));
-          }
-          model_transform = model_transform
-        .times( Mat4.translation([-20, -2,0]));
-        }
+        let model_transform = this.create_ground(graphics_state, Mat4.identity());
+        model_transform = this.create_wall(graphics_state, model_transform);
 
       }
   }
 
 
-// Extra credit begins here (See TODO comments below):
+window.Cube = window.classes.Cube =
+    class Cube extends Shape                 // Here's a complete, working example of a Shape subclass.  It is a blueprint for a cube.
+    { constructor()
+    { super( "positions", "normals" ); // Name the values we'll define per each vertex.  They'll have positions and normals.
 
-window.Ring_Shader = window.classes.Ring_Shader =
-class Ring_Shader extends Shader              // Subclasses of Shader each store and manage a complete GPU program.
-{ material() { return { shader: this } }      // Materials here are minimal, without any settings.
-  map_attribute_name_to_buffer_name( name )       // The shader will pull single entries out of the vertex arrays, by their data fields'
-    {                                             // names.  Map those names onto the arrays we'll pull them from.  This determines
-                                                  // which kinds of Shapes this Shader is compatible with.  Thanks to this function,
-                                                  // Vertex buffers in the GPU can get their pointers matched up with pointers to
-                                                  // attribute names in the GPU.  Shapes and Shaders can still be compatible even
-                                                  // if some vertex data feilds are unused.
-      return { object_space_pos: "positions" }[ name ];      // Use a simple lookup table.
+        // First, specify the vertex positions -- just a bunch of points that exist at the corners of an imaginary cube.
+        this.positions.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [-1,-1,1], [1,-1,1], [1,1,-1],  [-1,1,-1],  [1,1,1],  [-1,1,1],
+            [-1,-1,-1], [-1,-1,1], [-1,1,-1], [-1,1,1], [1,-1,1],  [1,-1,-1],  [1,1,1],  [1,1,-1],
+            [-1,-1,1],  [1,-1,1],  [-1,1,1],  [1,1,1], [1,-1,-1], [-1,-1,-1], [1,1,-1], [-1,1,-1] ) );
+        // Supply vectors that point away from eace face of the cube.  They should match up with the points in the above list
+        // Normal vectors are needed so the graphics engine can know if the shape is pointed at light or not, and color it accordingly.
+        this.normals.push(   ...Vec.cast( [0,-1,0], [0,-1,0], [0,-1,0], [0,-1,0], [0,1,0], [0,1,0], [0,1,0], [0,1,0], [-1,0,0], [-1,0,0],
+            [-1,0,0], [-1,0,0], [1,0,0],  [1,0,0],  [1,0,0], [1,0,0], [0,0,1], [0,0,1], [0,0,1],   [0,0,1],
+            [0,0,-1], [0,0,-1], [0,0,-1], [0,0,-1] ) );
+
+        // Those two lists, positions and normals, fully describe the "vertices".  What's the "i"th vertex?  Simply the combined
+        // data you get if you look up index "i" of both lists above -- a position and a normal vector, together.  Now let's
+        // tell it how to connect vertex entries into triangles.  Every three indices in this list makes one triangle:
+        this.indices.push( 0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+            14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22 );
+        // It stinks to manage arrays this big.  Later we'll show code that generates these same cube vertices more automatically.
     }
-    // Define how to synchronize our JavaScript's variables to the GPU's:
-  update_GPU( g_state, model_transform, material, gpu = this.g_addrs, gl = this.gl )
-      { const proj_camera = g_state.projection_transform.times( g_state.camera_transform );
-                                                                                        // Send our matrices to the shader programs:
-        gl.uniformMatrix4fv( gpu.model_transform_loc,             false, Mat.flatten_2D_to_1D( model_transform.transposed() ) );
-        gl.uniformMatrix4fv( gpu.projection_camera_transform_loc, false, Mat.flatten_2D_to_1D(     proj_camera.transposed() ) );
-      }
-  shared_glsl_code()            // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-    { return `precision mediump float;
-              varying vec4 position;
-              varying vec4 center;
-      `;
     }
-  vertex_glsl_code()           // ********* VERTEX SHADER *********
-    { return `
-        attribute vec3 object_space_pos;
-        uniform mat4 model_transform;
-        uniform mat4 projection_camera_transform;
-
-        void main()
-        {
-        }`;           // TODO:  Complete the main function of the vertex shader (Extra Credit Part II).
-    }
-  fragment_glsl_code()           // ********* FRAGMENT SHADER *********
-    { return `
-        void main()
-        {
-        }`;           // TODO:  Complete the main function of the fragment shader (Extra Credit Part II).
-    }
-}
-
-window.Grid_Sphere = window.classes.Grid_Sphere =
-class Grid_Sphere extends Shape           // With lattitude / longitude divisions; this means singularities are at
-  { constructor( rows, columns, texture_range )             // the mesh's top and bottom.  Subdivision_Sphere is a better alternative.
-      { super( "positions", "normals", "texture_coords" );
-
-
-                      // TODO:  Complete the specification of a sphere with lattitude and longitude lines
-                      //        (Extra Credit Part III)
-      } }
-
-      window.Cube = window.classes.Cube =
-      class Cube extends Shape                 // Here's a complete, working example of a Shape subclass.  It is a blueprint for a cube.
-        { constructor()
-            { super( "positions", "normals" ); // Name the values we'll define per each vertex.  They'll have positions and normals.
-
-              // First, specify the vertex positions -- just a bunch of points that exist at the corners of an imaginary cube.
-              this.positions.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [-1,-1,1], [1,-1,1], [1,1,-1],  [-1,1,-1],  [1,1,1],  [-1,1,1],
-                                                [-1,-1,-1], [-1,-1,1], [-1,1,-1], [-1,1,1], [1,-1,1],  [1,-1,-1],  [1,1,1],  [1,1,-1],
-                                                [-1,-1,1],  [1,-1,1],  [-1,1,1],  [1,1,1], [1,-1,-1], [-1,-1,-1], [1,1,-1], [-1,1,-1] ) );
-              // Supply vectors that point away from eace face of the cube.  They should match up with the points in the above list
-              // Normal vectors are needed so the graphics engine can know if the shape is pointed at light or not, and color it accordingly.
-              this.normals.push(   ...Vec.cast( [0,-1,0], [0,-1,0], [0,-1,0], [0,-1,0], [0,1,0], [0,1,0], [0,1,0], [0,1,0], [-1,0,0], [-1,0,0],
-                                                [-1,0,0], [-1,0,0], [1,0,0],  [1,0,0],  [1,0,0], [1,0,0], [0,0,1], [0,0,1], [0,0,1],   [0,0,1],
-                                                [0,0,-1], [0,0,-1], [0,0,-1], [0,0,-1] ) );
-
-                       // Those two lists, positions and normals, fully describe the "vertices".  What's the "i"th vertex?  Simply the combined
-                       // data you get if you look up index "i" of both lists above -- a position and a normal vector, together.  Now let's
-                       // tell it how to connect vertex entries into triangles.  Every three indices in this list makes one triangle:
-              this.indices.push( 0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
-                                14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22 );
-              // It stinks to manage arrays this big.  Later we'll show code that generates these same cube vertices more automatically.
-            }
-        }
--- tiny-graphics.js
+++ tiny-graphics.js
@@ -1,6 +1,62 @@
 // tiny-graphics.js - A file that shows how to organize a complete graphics program.
 // It wraps common WebGL commands, math, and web page interactions.  (by Garett)
 
+
+// const Texture = tiny.Texture =
+//     class Texture extends Graphics_Card_Object
+//     {                                             // **Texture** wraps a pointer to a new texture image where
+//                                                   // it is stored in GPU memory, along with a new HTML image object.
+//                                                   // This class initially copies the image to the GPU buffers,
+//                                                   // optionally generating mip maps of it and storing them there too.
+//       constructor( filename, min_filter = "LINEAR_MIPMAP_LINEAR" )
+//       { super();
+//         Object.assign( this, { filename, min_filter } );
+//         // Create a new HTML Image object:
+//         this.image          = new Image();
+//         this.image.onload   = () => this.ready = true;
+//         this.image.crossOrigin = "Anonymous";           // Avoid a browser warning.
+//         this.image.src = filename;
+//       }
+//       copy_onto_graphics_card( context, need_initial_settings = true )
+//       {                                     // copy_onto_graphics_card():  Called automatically as needed to load the
+//         // texture image onto one of your GPU contexts for its first time.
+//
+//         // Define what this object should store in each new WebGL Context:
+//         const initial_gpu_representation = { texture_buffer_pointer: undefined };
+//         // Our object might need to register to multiple GPU contexts in the case of
+//         // multiple drawing areas.  If this is a new GPU context for this object,
+//         // copy the object to the GPU.  Otherwise, this object already has been
+//         // copied over, so get a pointer to the existing instance.
+//         const gpu_instance = super.copy_onto_graphics_card( context, initial_gpu_representation );
+//
+//         if( !gpu_instance.texture_buffer_pointer ) gpu_instance.texture_buffer_pointer = context.createTexture();
+//
+//         const gl = context;
+//         gl.bindTexture  ( gl.TEXTURE_2D, gpu_instance.texture_buffer_pointer );
+//
+//         if( need_initial_settings )
+//         { gl.pixelStorei  ( gl.UNPACK_FLIP_Y_WEBGL, true );
+//           gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );         // Always use bi-linear sampling when zoomed out.
+//           gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl[ this.min_filter ]  );  // Let the user to set the sampling method
+//         }                                                                                    // when zoomed in.
+//
+//         gl.texImage2D   ( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.image );
+//         if( this.min_filter = "LINEAR_MIPMAP_LINEAR" )      // If the user picked tri-linear sampling (the default) then generate
+//           gl.generateMipmap(gl.TEXTURE_2D);                 // the necessary "mips" of the texture and store them on the GPU with it.
+//         return gpu_instance;
+//       }
+//       activate( context, texture_unit = 0 )
+//       {                                     // activate(): Selects this Texture in GPU memory so the next shape draws using it.
+//         // Optionally select a texture unit in case you're using a shader with many samplers.
+//         // Terminate draw requests until the image file is actually loaded over the network:
+//         if( !this.ready )
+//           return;
+//         const gpu_instance = super.activate( context );
+//         context.activeTexture( context[ "TEXTURE" + texture_unit ] );
+//         context.bindTexture( context.TEXTURE_2D, gpu_instance.texture_buffer_pointer );
+//       }
+//     }
+
 window.Vec = window.tiny_graphics.Vec =
 class Vec extends Float32Array        // Vectors of floating point numbers.  This puts vector math into JavaScript.
                                       // See these examples for usage of each function:
